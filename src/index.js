75bfa220258ae0c30e0be1d22a510a32603dc98d
rich <adukyerer@gmail.com>
2022-06-02 14:49:12 +0000

    add object

--- src/index.js
+++ src/index.js
@@ -1,2 +1,43 @@
 /* eslint-disable linebreak-style */
 import './styles/style.css';
+
+const descriptions = [
+  {
+    id: 1,
+    description: 'pray',
+    completed: true,
+  },
+  {
+    id: 2,
+    description: 'Read historybook',
+    completed: true,
+  },
+  {
+    id: 3,
+    description: 'Morning Walk',
+    completed: false,
+  },
+  {
+    id: 4,
+    description: '30 Squats',
+    completed: false,
+  },
+];
+
+const toDo = document.getElementById('todo-list');
+
+function TaskObject() {
+  let workout = '';
+  descriptions.forEach((job) => {
+    workout += `
+              <div class="list">
+                  <input type="checkbox">
+                  <p>${job.description}</p>
+             
+         <a><i class="fa fa-ellipsis-v fa-2x" aria-hidden="true"></i>
+                  </a>
+              </div>`;
+  });
+  toDo.innerHTML = workout;
+}
+TaskObject();
